ac19bf6981656f072eaf385b174a4f2cd81a9351
tuxknrp <tuxknrp@gmail.com>
2021-07-31 00:31:11 -0500

    updated icons

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
 
 // projects
-import L_ReactToDoList from "../../assets/img/projects/React_ToDo_List.webp";
-import L_MernTipCalc from "../../assets/img/projects/mern_tip_calc.webp";
-import L_GetGitHubInfo from "../../assets/img/projects/get_github_info.webp";
-import L_SmartBrain from "../../assets/img/projects/brain.webp";
-import L_RoboFriends from "../../assets/img/projects/Robofriends.webp";
-import L_ProductHuntClone from "../../assets/img/projects/product_hunt_clone.webp";
-import L_PortfolioUsingDjango from "../../assets/img/projects/portfolioUsingDjango.webp";
 import L_Tiomanga from "../../assets/img/projects/tiomanga.webp";
 import L_Vierbeinig from "../../assets/img/projects/vierbeinig.webp";
 import L_Mapacovid from "../../assets/img/projects/mapacovid.webp";
@@ -19,10 +12,6 @@ import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 
 // skills
-import L_REACT from "../../assets/img/skills/react.svg";
-import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
-import L_EXPRESS from "../../assets/img/skills/express.svg";
-import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
 import L_MONGODB from "../../assets/img/skills/mongodb.svg";
 import Image from "react-bootstrap/Image";
 import L_REDUX from "../../assets/img/skills/redux.svg";
@@ -30,9 +19,6 @@ import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
 import L_BOOTSTRAP4 from "../../assets/img/skills/bootstrap-4.svg";
 import L_DJANGO from "../../assets/img/skills/django.svg";
-import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
-import L_GIT from "../../assets/img/skills/github-api.svg";
-import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
 import L_PHP from "../../assets/img/skills/php.svg";
 import L_AWS from "../../assets/img/skills/aws.svg";
 import L_LARAVEL from "../../assets/img/skills/laravel-1.svg";
--- src/pages/skills/skills-data.js
+++ src/pages/skills/skills-data.js
@@ -3,34 +3,28 @@ import L_CSS3 from "../../assets/img/skills/css3.svg";
 import L_SASS from "../../assets/img/skills/sass-1.svg";
 // import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
 import L_REACT from "../../assets/img/skills/react.svg";
-import L_REDUX from "../../assets/img/skills/redux.svg";
+// import L_REDUX from "../../assets/img/skills/redux.svg";
 // import L_REACT_ROUTER from "../../assets/img/skills/react-router.svg";
-// import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
 // import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
 // import L_STYLED_COMPONENTS from "../../assets/img/skills/styled-components.svg";
 import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
-import L_EXPRESS from "../../assets/img/skills/express.svg";
+// import L_EXPRESS from "../../assets/img/skills/express.svg";
 import L_DJANGO from "../../assets/img/skills/django.svg";
 // import L_FLASK from "../../assets/img/skills/flask.svg";
 import L_MONGODB from "../../assets/img/skills/mongodb.svg";
 import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
-// import L_MSSQL from "../../assets/img/skills/mssql.svg";
 import L_GIT from "../../assets/img/skills/git-icon.svg";
 import L_HEROKU from "../../assets/img/skills/heroku.svg";
-import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
+// import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
 import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
 import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
 import L_TYPESCRIPT from "../../assets/img/skills/typescript.svg";
-import L_GOLANG from "../../assets/img/skills/go-6.svg";
 import L_PYTHON from "../../assets/img/skills/python.svg";
 
 //frontend
 import L_REACTNATIVE from "../../assets/img/skills/react-native-firebase-1.svg";
-import L_ANGULAR from "../../assets/img/skills/angular-icon-1.svg";
-import L_FLUTTER from "../../assets/img/skills/flutter-logo.svg";
 import L_ANDROID from "../../assets/img/skills//android.svg";
-import L_IOS from "../../assets/img/skills/ios-2.svg";
-import L_VUE from "../../assets/img/skills/vue.svg";
+// import L_IOS from "../../assets/img/skills/ios-2.svg";
 
 //CI/CD TOOLS
 import L_DOCKER from "../../assets/img/skills/docker.svg";
@@ -45,7 +39,6 @@ import L_WINDOWS from "../../assets/img/skills/microsoft_windows.svg";
 //backend
 import L_LARAVEL from "../../assets/img/skills/laravel-1.svg";
 import L_NESTJS from "../../assets/img/skills/nestjs.svg";
-import L_SPRING from "../../assets/img/skills/spring-3.svg";
 import L_ARDUINO from "../../assets/img/skills/arduino.svg";
 
 //hosting
@@ -54,24 +47,16 @@ import L_PYTHONANYWHERE from "../../assets/img/skills/pythonanywhere.svg";
 
 //Version control
 import L_MERCURIAL from "../../assets/img/skills/mercurial.svg";
-import L_GITHUB from "../../assets/img/skills/github_2.svg";
-import L_GITLAB from "../../assets/img/skills/gitlab.svg";
 
 //Data Analytics
 import L_BI from "../../assets/img/skills/BI.png";
 import L_ML from "../../assets/img/skills/ML.png";
-import L_SCIKITLEARN from "../../assets/img/skills/scikitlearn.png";
+import L_SCIKITLEARN from "../../assets/img/skills/sklearn.png";
 import L_NUMPY from "../../assets/img/skills/numpy.svg";
 import L_PANDAS from "../../assets/img/skills/pandas.png";
 
 
-//ERP
-// import L_SAP from "../../assets/img/skills/sap-2011-logo.svg";
-
 //Languages
-import L_C from "../../assets/img/skills/c--4.svg";
-import L_JAVA4 from "../../assets/img/skills/java-4.svg";
-import L_SWIFT from "../../assets/img/skills/swift-15.svg";
 import L_PHP from "../../assets/img/skills/php.svg";
 import L_SHELL from "../../assets/img/skills/shell.png";
 
@@ -80,9 +65,9 @@ import L_FIREBASE from "../../assets/img/skills/firebase-1.svg";
 import L_MYSQL from "../../assets/img/skills/mysql.svg";
 
 //Testing & Clean Code
-import L_JEST from "../../assets/img/skills/jest.png";
+import L_JEST from "../../assets/img/skills/jest.svg";
 import L_TDD from "../../assets/img/skills/TDD.png";
-import L_OOP from "../../assets/img/skills/OOP.jpg";
+import L_OOP from "../../assets/img/skills/oop.png";
 import L_SOLID from "../../assets/img/skills/SOLID.png";
 
 export const skills = {
@@ -100,18 +85,18 @@ export const skills = {
       imgSrc: L_CSS3,
       skillName: "CSS3",
     },
-    {
-      link: "https://www.javascript.com/",
-      imgAltText: "JavaScript",
-      imgSrc: L_JAVASCRIPT,
-      skillName: "JavaScript",
-    },
-    {
-      link: "https://sass-lang.com/",
-      imgAltText: "Sass",
-      imgSrc: L_SASS,
-      skillName: "Sass",
-    },
+    // {
+    //   link: "https://www.javascript.com/",
+    //   imgAltText: "JavaScript",
+    //   imgSrc: L_JAVASCRIPT,
+    //   skillName: "JavaScript",
+    // },
+    // {
+    //   link: "https://sass-lang.com/",
+    //   imgAltText: "Sass",
+    //   imgSrc: L_SASS,
+    //   skillName: "Sass",
+    // },
     // {
     //   link: "https://getbootstrap.com/",
     //   imgAltText: "Bootstrap",
@@ -149,46 +134,16 @@ export const skills = {
     //   skillName: "Material-UI",
     // },
     // {
-    //   link: "https://react-bootstrap.github.io/",
-    //   imgAltText: "React Bootstrap",
-    //   imgSrc: L_REACT_BOOTSTRAP,
-    //   skillName: "React Bootstrap",
-    // },
-    // {
     //   link: "https://styled-components.com/",
     //   imgAltText: "styled-components",
     //   imgSrc: L_STYLED_COMPONENTS,
     //   skillName: "styled-components",
-    // },
-    // {
-    //   link: "https://angular.io/",
-    //   imgAltText: "angular",
-    //   imgSrc: L_ANGULAR,
-    //   skillName: "Angular",
-    // },
-    // {
-    //   link: "https://flutter.dev/",
-    //   imgAltText: "Flutter",
-    //   imgSrc: L_FLUTTER,
-    //   skillName: "Flutter",
     // },
     {
       link: "https://developer.android.com/",
       imgAltText: "Android",
       imgSrc: L_ANDROID,
       skillName: "Android",
-    },
-    {
-      link: "https:developer.apple.com/swift/",
-      imgAltText: "IOS",
-      imgSrc: L_IOS,
-      skillName: "IOS",
-    },
-    {
-      link: "https://vuejs.org/",
-      imgAltText: "Vuejs",
-      imgSrc: L_VUE,
-      skillName: "Vuejs",
     },
   ],
 
@@ -271,12 +226,6 @@ export const skills = {
       imgSrc: L_NESTJS,
       skillName: "NestJS",
     },
-    // {
-    //   link: "https://spring.io/",
-    //   imgAltText: "Spring",
-    //   imgSrc: L_SPRING,
-    //   skillName: "Spring",
-    // },
     {
       link: "https://www.arduino.cc/",
       imgAltText: "Arduino",
@@ -365,14 +314,6 @@ export const skills = {
     },
   ],
 
-  // erp: [
-  //   {
-  //     link: "https://www.sap.com/latinamerica/index.html",
-  //     imgAltText: "SAP",
-  //     imgSrc: L_SAP,
-  //     skillName: "SAP R/3",
-  //   },
-  // ],
   programmingLanguages: [
     {
       link: "https://www.python.org/",
@@ -392,24 +333,6 @@ export const skills = {
       imgSrc: L_TYPESCRIPT,
       skillName: "TypeScript",
     },
-    // {
-    //   link: "https://golang.org/",
-    //   imgAltText: "Golang",
-    //   imgSrc: L_GOLANG,
-    //   skillName: "Golang",
-    // },
-    {
-      link: "https://www.java.com/es/",
-      imgAltText: "Java",
-      imgSrc: L_JAVA4,
-      skillName: "Java",
-    },
-    {
-      link: "https:www.apple.com/es/swift/",
-      imgAltText: "Swift",
-      imgSrc: L_SWIFT,
-      skillName: "Swift",
-    },
     {
       link: "https://www.php.net/",
       imgAltText: "PHP",
@@ -422,12 +345,6 @@ export const skills = {
       imgSrc: L_SHELL,
       skillName: "Bash - Shell Scripting",
     },
-    // {
-    //   link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
-    //   imgAltText: "C#",
-    //   imgSrc: L_C,
-    //   skillName: "C#",
-    // },
   ],
   databases: [
     {
